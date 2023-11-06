c3183f9d990fbd79564299b8011020ac5981c51e
Aryanop0914 <21cs038@charusat.edu.in>
2023-11-06 22:25:57 +0530

    Api Error utils

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./.env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server Running on Port : ${process.env.PORT}`);
+    });
+    console.log("MongoDB connected Succesfull");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed", err);
+  });
