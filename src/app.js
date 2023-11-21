0e50577f81e31d2fd67dece7196e21a901ed13f8
Aryanop0914 <21cs038@charusat.edu.in>
2023-11-21 19:48:14 +0530

    register controller

--- src/app.js
+++ src/app.js
@@ -16,4 +16,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
+import chalk from "chalk";
 dotenv.config({ path: "./.env" });
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 4000, () => {
-      console.log(`Server Running on Port : ${process.env.PORT}`);
+      console.log(chalk.green(`Server Running on Port : ${process.env.PORT}`));
     });
-    console.log("MongoDB connected Succesfull");
+    console.log(chalk.green("MongoDB connected Succesfull"));
   })
   .catch((err) => {
-    console.log("MongoDB Connection Failed", err);
+    console.log(chalk.red("MongoDB Connection Failed", err));
   });
