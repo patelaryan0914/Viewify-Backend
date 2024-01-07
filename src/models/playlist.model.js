608510caebd4f54ed81bd991d66bc33efe242cf0
Aryanop0914 <21cs038@charusat.edu.in>
2024-01-07 22:13:42 +0530

    All Models Done

---
+++ src/models/playlist.model.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videos: [{ type: Schema.Types.ObjectId, ref: "Video" }],
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+export const Playlist = model("Playlist", playlistSchema);
---
+++ src/models/tweet.model.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+
+const tweetSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Tweet = model("Tweet", tweetSchema);
