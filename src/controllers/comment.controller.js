6b6943b8c75d7f599cf2c1915c1066abf17c9a8e
Aryanop0914 <21cs038@charusat.edu.in>
2024-01-10 22:19:24 +0530

    comment routes done

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -12,14 +12,55 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
   // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+  const userId = req.user?._id;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "VideoId is missing or corrupted");
+  }
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: userId,
+  });
+  if (!comment) {
+    throw new ApiError(400, "Something Went Wrong While Commenting");
+  }
+
+  return res.status(200).json(new ApiResponse(200, "Comment Created"));
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  const { commentId } = req.params;
+  const { updatedContent } = req.body;
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Comment Id is missing or corrupted");
+  }
+  const updatedComment = await Comment.findByIdAndUpdate(commentId, {
+    $set: { content: updatedContent },
+  });
+  if (!updatedComment) {
+    throw new ApiError(400, "Something Went Wrong While Updating Comment");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Comment Updated Succesfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+  const { commentId } = req.params;
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Comment Id is missing or corrupted");
+  }
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
+  if (!deletedComment) {
+    throw new ApiError(400, "Something Went Wrong While Deleting Comment");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Comment Deleted Succesfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
